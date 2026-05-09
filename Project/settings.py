2f274b424985043231371efee0bee9303aeb4002
Amrit Chaudhary <amritchaudhary620@gmail.com>
2026-05-09 17:38:33 +0545

    Updated project changes

--- Project/settings.py
+++ Project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4@ym$lr(z+&!4#&4a5&o=jz*jo9)ywe%z+xas_)i%pz2w3o8&)"
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = [
+    host for host in os.getenv("ALLOWED_HOSTS", "").split(",") if host
+]
 
 # Application definition
 
